25c60a75b83751bd2751df92869db02f17a76c20
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-08-09 17:47:49 +0300

    errors

--- middleware/errorHandlers.js
+++ middleware/errorHandlers.js
@@ -1,3 +1,26 @@
+const { ValidationError, BaseError } = require('sequelize');
+
+module.exports.dbErrorHandler = (err, req, res, next) => {
+  // Відловлюємо помилки Sequelize (https://sequelize.org/api/v6/identifiers.html#errors)
+
+  // Помилки валідації (невалідне ім'я, рік народження з майбутнього, ...)
+  if (err instanceof ValidationError) {
+    const errors = err.errors.map((e) => ({ status: 422, title: e.message }));
+    // згідно з json:api запаковуємо одразу корисну інфу з об'єкту помилки
+    // одразу в масив об'єктів з полями status, message
+    return res.status(422).send(errors);
+  }
+
+  // Аналогічно можна прописати обробку інших типів помилок Sequelize
+  // ...
+
+  // Всі інші окремо не оброблені помилки Sequelize:
+  if (err instanceof BaseError) {
+    next(createHttpError(500, 'Database Error'));
+  }
+  next(err);
+};
+
 module.exports.errorHandler = (err, req, res, next) => {
   if (res.headersSent) {
     return;
@@ -6,7 +29,7 @@ module.exports.errorHandler = (err, req, res, next) => {
   const status = err.status || 500;
   const message = err.message || 'Server Error';
 
-  res.status(status).send([{status:status, message}]);
+  res.status(status).send([{ status: status, message }]);
 };
 
 // [
