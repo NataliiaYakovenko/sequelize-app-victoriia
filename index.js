300afd348f8a07fc43eab3bb83641c2ea430c4f6
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-08-14 16:50:54 +0300

    .ENV

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const {sequelize,User,Group} = require('./models')
-
 const http = require('http');
 const app = require('./app');
+// require('dotenv').config()
 
 const PORT = process.env.PORT || 5000;
 const HOST = process.env.HOST || '127.0.0.1';
@@ -11,6 +10,8 @@ const httpServer = http.createServer(app);
 httpServer.listen(PORT, HOST, () =>
   console.log(`Server is listening http://${HOST}:${PORT}`)
 );
+
+console.log(process.env)
 
 //------------------------------------------------------------------
 
